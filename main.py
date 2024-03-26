77dab840a25f599a91ce04eb387597b64fd73d6a
Rayder <Rayder.chance@gmail.com>
2024-03-26 13:37:53 -0400

    Update

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
-import db_manager
 import thread_manager
 
 from flask import Flask, render_template
 
-thread_manager.start()
+# thread_manager.start()
 
 app = Flask(__name__)
 app.secret_key = "secret key"
 
-db_manager.update("db/manga_downloads.db", "manga", "name", "name", 1)
-db_manager.list_all("db/manga_downloads.db", "manga")
-
 @app.route('/')
 def home_page():
-    return render_template('index.html')
+    return render_template('index.html', title="Home")
 
 
 @app.route('/database/manga')
 def database_manga():
-    return render_template("manga_db.html")
+    return render_template("manga_db.html", title="Manga db")
+
+@app.route('/manga.read')
+def read_manga():
+    return render_template("read_manga.html", title="Manga Reader")
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False)
+    app.run(host='0.0.0.0', debug=True)
 
 # Ends the program pretty much
 thread_manager.stop()
